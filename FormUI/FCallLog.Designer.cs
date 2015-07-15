c292382e54a4e35bcf5126053eb92446da251f30
zhangzhijie1988 <zhangzhijie1988@126.com>
2015-07-15 14:04:39 +0800

    calllog提交

---
+++ FormUI/FCallLog.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace FormUI
+{
+    partial class FCallLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FCallLog));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDate1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpDate2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bdsCallLog = new System.Windows.Forms.BindingSource(this.components);
+            this.txtExtensionNo = new System.Windows.Forms.TextBox();
+            this.SeatPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TalkTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stpCallLog = new FormUI.SplitPage();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsCallLog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtExtensionNo);
+            this.panel1.Controls.Add(this.dtpDate2);
+            this.panel1.Controls.Add(this.dtpDate1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "时间>=";
+            // 
+            // dtpDate1
+            // 
+            this.dtpDate1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate1.Location = new System.Drawing.Point(60, 13);
+            this.dtpDate1.Name = "dtpDate1";
+            this.dtpDate1.Size = new System.Drawing.Size(91, 21);
+            this.dtpDate1.TabIndex = 1;
+            this.dtpDate1.ValueChanged += new System.EventHandler(this.dtpDate_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "时间<=";
+            // 
+            // dtpDate2
+            // 
+            this.dtpDate2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate2.Location = new System.Drawing.Point(217, 13);
+            this.dtpDate2.Name = "dtpDate2";
+            this.dtpDate2.Size = new System.Drawing.Size(91, 21);
+            this.dtpDate2.TabIndex = 1;
+            this.dtpDate2.ValueChanged += new System.EventHandler(this.dtpDate_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "分机号";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(581, 633);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.stpCallLog);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 584);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(581, 49);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dataGridView1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(581, 584);
+            this.panel4.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SeatPhone,
+            this.Phone,
+            this.StartTime,
+            this.TalkTime});
+            this.dataGridView1.DataSource = this.bdsCallLog;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 584);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // txtExtensionNo
+            // 
+            this.txtExtensionNo.Location = new System.Drawing.Point(369, 14);
+            this.txtExtensionNo.Name = "txtExtensionNo";
+            this.txtExtensionNo.Size = new System.Drawing.Size(100, 21);
+            this.txtExtensionNo.TabIndex = 2;
+            this.txtExtensionNo.TextChanged += new System.EventHandler(this.txtExtensionNo_TextChanged);
+            // 
+            // SeatPhone
+            // 
+            this.SeatPhone.DataPropertyName = "SeatPhone";
+            this.SeatPhone.HeaderText = "分机号";
+            this.SeatPhone.Name = "SeatPhone";
+            this.SeatPhone.ReadOnly = true;
+            // 
+            // Phone
+            // 
+            this.Phone.DataPropertyName = "Phone";
+            this.Phone.HeaderText = "通话号码";
+            this.Phone.Name = "Phone";
+            this.Phone.ReadOnly = true;
+            // 
+            // StartTime
+            // 
+            this.StartTime.DataPropertyName = "StartTime";
+            this.StartTime.HeaderText = "接通时间";
+            this.StartTime.Name = "StartTime";
+            this.StartTime.ReadOnly = true;
+            this.StartTime.Width = 120;
+            // 
+            // TalkTime
+            // 
+            this.TalkTime.DataPropertyName = "TalkTime";
+            this.TalkTime.HeaderText = "通话时间";
+            this.TalkTime.Name = "TalkTime";
+            this.TalkTime.ReadOnly = true;
+            // 
+            // stpCallLog
+            // 
+            this.stpCallLog.IsTwoRow = false;
+            this.stpCallLog.Location = new System.Drawing.Point(81, 11);
+            this.stpCallLog.Name = "stpCallLog";
+            this.stpCallLog.PageCurrent = 1;
+            this.stpCallLog.PageSize = 20;
+            this.stpCallLog.RecordCount = 0;
+            this.stpCallLog.Size = new System.Drawing.Size(419, 30);
+            this.stpCallLog.TabIndex = 0;
+            this.stpCallLog.EventPaging += new FormUI.SplitPage.EventPagingHandler(this.splitPage1_EventPaging);
+            // 
+            // FCallLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 681);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(589, 708);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(589, 708);
+            this.Name = "FCallLog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "通话详细";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsCallLog)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dtpDate1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDate2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bdsCallLog;
+        private System.Windows.Forms.Panel panel3;
+        private SplitPage stpCallLog;
+        private System.Windows.Forms.TextBox txtExtensionNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SeatPhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StartTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TalkTime;
+    }
+}
---
+++ FormUI/FCallLog.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ad.Model.VModel;
+using Ad.Model.DbModel;
+using Ad.Fw;
+using FormUI.Tool;
+
+namespace FormUI
+{
+    public partial class FCallLog : Form
+    {
+        public FCallLog()
+        {
+            InitializeComponent();
+            InitControl();
+        }
+
+        private void InitControl()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            int year = currentDateTime.Year;
+            int month = currentDateTime.Month;
+            int day = currentDateTime.Day;
+            this.dtpDate1.Value = new DateTime(year, month, day, 0, 0, 0);
+            this.dtpDate2.Value = new DateTime(year, month, day, 23, 59, 59);
+
+            this.stpCallLog.PageSize = 50;
+
+            this.txtExtensionNo.Text = AnswerHelper.CurrentSeatPhone;
+            if (!LoginHelper.IsGodUser && !LoginHelper.IsHasFunction(EnumHelper.FunctionEnum.Special))
+            {
+                this.txtExtensionNo.Enabled = false;
+            }
+
+            BindingCallLog();
+        }
+
+        // 分页
+        private void splitPage1_EventPaging(SplitPage.EventPagingArg e)
+        {
+            BindingCallLog();
+        }
+
+        private void BindingCallLog()
+        {
+            VCallLog model = new VCallLog();
+            model.ExtensionNum = this.txtExtensionNo.Text.Trim();
+            model.StartTime1 = this.dtpDate1.Value;
+            model.StartTime2 = this.dtpDate2.Value;
+            string orderString = string.Format("{0} asc,{1} asc", Y_Call_Log_Description.SeatPhone, Y_Call_Log_Description.StartTime);
+            var pageList = BllCallLog.SelectSplit(model, this.stpCallLog.PageSize, this.stpCallLog.PageCurrent, orderString);
+            this.bdsCallLog.DataSource = pageList.Models;
+            this.stpCallLog.RecordCount = pageList.RecordCount;
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            int rownum = (e.RowIndex + 1);
+            Rectangle rct = new Rectangle(e.RowBounds.Location.X, e.RowBounds.Location.Y + 4, ((DataGridView)sender).RowHeadersWidth - 4, e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics, rownum.ToString(), ((DataGridView)sender).RowHeadersDefaultCellStyle.Font, rct, 
+                ((DataGridView)sender).RowHeadersDefaultCellStyle.ForeColor, System.Drawing.Color.Transparent, TextFormatFlags.HorizontalCenter);
+        }
+        private void dtpDate_ValueChanged(object sender, EventArgs e)
+        {
+            BindingCallLog();
+        }
+
+        private void txtExtensionNo_TextChanged(object sender, EventArgs e)
+        {
+            BindingCallLog();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            BindingCallLog();
+        }
+    }
+}
